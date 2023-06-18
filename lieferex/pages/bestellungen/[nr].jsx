4a3ac5c68f7f8764ed743783742a6d78ce20c356
Hamzenis Kryeziu <hamzenis@gmail.com>
2023-06-18 17:41:22 +0200

    Last Test inshalla it will work

--- lieferex/pages/bestellungen/[nr].jsx
+++ lieferex/pages/bestellungen/[nr].jsx
@@ -104,7 +104,7 @@ export default function Bestellung({ bestellung }) {
 }
 
 export async function getServerSideProps({ params }) {
-    const res = await axios.get(`http://localhost:3000/api/bestellungen/${params.nr}`);
+    const res = await axios.get(`https://liefer-ex.de/api/bestellungen/${params.nr}`);
     return {
         props: { bestellung: res.data },
     };
